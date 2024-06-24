0bf8cd0f8de6e0b55ee17628d09d496b1bb71ce3
Usama-Latif <usamalatif5055@gmail.com>
2024-06-24 22:36:58 +0500

    problem 8 commit 2

--- problem8.js
+++ problem8.js
@@ -1,19 +1,36 @@
 // write a function factorial that takes a non-negative number as input and return its factorial
 
-let num = 5;
 
+// let num = 5;
+
+// function factorial(num) {
+//     if (num == 0 || num == 1) {
+//         return 1;
+//     }
+//     else {
+//         let f = 1;
+//         for (let i = 2; i <= num; i++) {
+//             f = f * i;
+//         }
+//         return f;
+//     }
+
+// }
+
+// console.log(factorial(num));
+
+
+
+
+// Factorial using recursion
 function factorial(num) {
-    if (num == 0 || num == 1) {
+    if (num == 1 || num == 0) {
         return 1;
     }
     else {
-        let f = 1;
-        for (let i = 2; i <= num; i++) {
-            f = f * i;
-        }
-        return f;
+        return num * factorial(num - 1);
     }
-
 }
 
-console.log(factorial(num));
+
+console.log(factorial(6));
